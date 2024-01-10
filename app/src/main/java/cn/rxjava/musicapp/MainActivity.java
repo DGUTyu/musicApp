86e539213527d57577995ab6270dc8118cf2ca89
yujiajian <jiajian.yu@swiftpass.cn>
2024-01-10 17:44:22 +0800

    create project

---
+++ app/src/main/java/cn/rxjava/musicapp/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.rxjava.musicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
